118e0becb735fba5806fa17560f72a8d1c64fa18
dsuraj <deshpande.suraj@gmail.com>
2016-03-16 22:30:51 +1100

    Initial commit

---
+++ Lorem Ipsum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lorem_Ipsum.Startup))]
+namespace Lorem_Ipsum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
